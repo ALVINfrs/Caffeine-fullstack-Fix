5d3ff908dc48b368b64f8b0240f050763aed278d
ALVINfrs <112801784+ALVINfrs@users.noreply.github.com>
2025-06-03 07:10:26 +0700

    fix: sesion 2

--- Backend/config/session.js
+++ Backend/config/session.js
@@ -1,36 +1,11 @@
 require("dotenv").config();
 const session = require("express-session");
-const RedisStore = require("connect-redis")(session);
-const redis = require("redis");
-
-// Create Redis client
-const client = redis.createClient({
-  url: process.env.REDIS_URL || "redis://localhost:6379",
-});
-
-// Connect to Redis (important!)
-client.connect().catch(console.error);
-
-// Handle connection events
-client.on("error", (err) => {
-  console.log("Redis Client Error", err);
-});
-
-client.on("connect", () => {
-  console.log("✅ Connected to Redis");
-});
 
 const sessionConfig = session({
-  store: new RedisStore({ client: client }),
   secret: process.env.SESSION_SECRET,
   resave: false,
-  saveUninitialized: false,
-  cookie: {
-    secure: true, // auto detect
-    maxAge: 24 * 60 * 60 * 1000,
-    httpOnly: true,
-    sameSite: "none", // untuk cross-origin
-  },
+  saveUninitialized: true,
+  cookie: { secure: false, maxAge: 24 * 60 * 60 * 1000 },
 });
 
 module.exports = sessionConfig;
