cb8e6d458412644e42f77bafe804f73df8e7141e
ALVINfrs <112801784+ALVINfrs@users.noreply.github.com>
2025-06-03 07:31:35 +0700

    fix: sesion 7

--- Backend/config/session.js
+++ Backend/config/session.js
@@ -1,38 +1,14 @@
-require("dotenv").config();
 const session = require("express-session");
-const RedisStore = require("connect-redis").default; // ✅ fix di v6
-const redis = require("redis");
 
-// Buat Redis client
-const client = redis.createClient({
-  url: process.env.REDIS_URL || "redis://localhost:6379",
-});
-
-client.connect().catch(console.error);
-
-// Event listener
-client.on("error", (err) => {
-  console.error("Redis Client Error", err);
-});
-
-client.on("connect", () => {
-  console.log("✅ Connected to Redis");
-});
-
-// Konfigurasi session
 const sessionConfig = session({
-  store: new RedisStore({
-    client: client,
-    prefix: "caffeine_session:",
-  }),
-  secret: process.env.SESSION_SECRET,
+  secret: process.env.SESSION_SECRET || "defaultsecret",
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: true,
-    maxAge: 24 * 60 * 60 * 1000,
-    httpOnly: true,
-    sameSite: "none",
+    maxAge: 24 * 60 * 60 * 1000, // 1 hari
+    secure: true, // cookie hanya dikirim lewat HTTPS
+    httpOnly: true, // mencegah akses cookie dari JavaScript di browser
+    sameSite: "none", // izinkan cookie dikirim dari cross-site (wajib pakai HTTPS)
   },
 });
 
