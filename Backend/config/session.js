b85b110f24c319f3f36f140b7a974648cf707e1c
ALVINfrs <112801784+ALVINfrs@users.noreply.github.com>
2025-06-03 07:39:39 +0700

    fix: sesion 8

--- Backend/config/session.js
+++ Backend/config/session.js
@@ -1,14 +1,19 @@
 const session = require("express-session");
+const FileStore = require("session-file-store")(session);
 
 const sessionConfig = session({
+  store: new FileStore({
+    path: "./sessions",
+    retries: 0,
+  }),
   secret: process.env.SESSION_SECRET || "defaultsecret",
   resave: false,
   saveUninitialized: false,
   cookie: {
     maxAge: 24 * 60 * 60 * 1000, // 1 hari
-    secure: true, // cookie hanya dikirim lewat HTTPS
-    httpOnly: true, // mencegah akses cookie dari JavaScript di browser
-    sameSite: "none", // izinkan cookie dikirim dari cross-site (wajib pakai HTTPS)
+    secure: true, // WAJIB true kalau sudah pakai HTTPS
+    httpOnly: true,
+    sameSite: "none", // WAJIB "none" kalau frontend beda domain dan pakai HTTPS
   },
 });
 
